1f3198fb802f5f83b81b7783947a96070931a3ed
Scott Reeves <scott.reeves14@det.nsw.edu.au>
2025-11-06 20:53:52 +0000

    Changed default database file and location. Now in ./data directory

--- movies/settings.py
+++ movies/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'movies.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'movies_db.sqlite3',
     }
 }
 
