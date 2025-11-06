9d433ae1131bdeb6f07b41657912bb473d95a002
Scott Reeves <scott.reeves14@det.nsw.edu.au>
2025-11-06 21:24:03 +0000

    Added in home.html page

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from .views import home_page_view, about_page_view
 
 urlpatterns = [
         path("about/", about_page_view),
+        path("home/", about_page_view),
         path("", home_page_view),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page_view(request):
-    return HttpResponse("Movies!")
+    return render(request, "pages/home.html")
 
 def about_page_view(request):
     return render(request, "pages/about.html")
